8d9e7aab1d13c54b6c737074e062c02bd81ce7d6
MELISSA JIMENEZ <melissa.jimenez.callisaya@gmail.com>
2022-10-22 09:25:21 -0400

    Project source code and execution video

---
+++ Mod4VulnerabilityMelJimenez/src/test/java/factoryBrowser/Chrome.java
@@ -0,0 +1,16 @@
+package factoryBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Chrome implements IBrowser{
+    @Override
+    public WebDriver create() {
+        System.setProperty("webdriver.chrome.driver","src/test/resources/driver/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver()  ;
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        return driver;
+    }
+}
